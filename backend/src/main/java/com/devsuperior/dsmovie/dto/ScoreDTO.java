ad476fe1dc0d994b313c933fc6285ecfb975184f
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-01-15 11:13:31 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/ScoreDTO.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dsmovie.dto;
+
+public class ScoreDTO {
+	
+	private Long MovieId;
+	private String email;
+	private Double score;
+	
+	public ScoreDTO() {
+	}
+
+	public Long getMovieId() {
+		return MovieId;
+	}
+
+	public void setMovieId(Long MovieId) {
+		this.MovieId = MovieId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+}
