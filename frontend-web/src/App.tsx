a5584cc3c39fee741fe27937fdb1a7b3ac0e4b97
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2023-12-29 18:20:07 -0300

    feat(FormCard): component created with route

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,9 +1,17 @@
-import { Navbar } from './component';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FormCard, Navbar } from './component';
 
 export const App = () => {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/form">
+            <Route path=":movieId" element={<FormCard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
--- frontend-web/src/component/index.ts
+++ frontend-web/src/component/index.ts
@@ -1,1 +1,2 @@
 export { Navbar } from './navbar/Navbar';
+export { FormCard } from './form-card/FormCard';
