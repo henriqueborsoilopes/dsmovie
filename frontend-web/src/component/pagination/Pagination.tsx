32bbfed32359d9397f9ff51f1c25fd75d6a84e53
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2023-12-29 21:32:13 -0300

    feat(Pagination): componente created

---
+++ frontend-web/src/component/pagination/Pagination.tsx
@@ -0,0 +1,18 @@
+import './Pagination.css';
+import arrow from '../../assets/img/arrow.svg';
+
+export const Pagination = () => {
+  return (
+    <div className="dsmovie-pagination-container">
+      <div className="dsmovie-pagination-box">
+        <button className="dsmovie-pagination-button" disabled={true}>
+          <img src={arrow} alt="Arrow" />
+        </button>
+        <p>{`${1} de ${3}`}</p>
+        <button className="dsmovie-pagination-button" disabled={false}>
+          <img src={arrow} alt="Arrow" className="dsmovie-flip-horizontal" />
+        </button>
+      </div>
+    </div>
+  );
+};
