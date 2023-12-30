aa63cb5f0428c3f0091290ef9b6e7ae248394d5f
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2023-12-29 21:42:56 -0300

    refactor(App): change route element

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FormCard, Navbar } from './component';
+import { Form, Listing } from './page';
+import { Navbar } from './component';
 
 export const App = () => {
   return (
@@ -7,8 +8,9 @@ export const App = () => {
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Route path="/" element={<Listing />} />
           <Route path="/form">
-            <Route path=":movieId" element={<FormCard />} />
+            <Route path=":movieId" element={<Form />} />
           </Route>
         </Routes>
       </BrowserRouter>
